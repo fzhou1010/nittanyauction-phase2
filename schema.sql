a78079c87bd00a5c287d5bafcbbf319d4cf84753
fzhou1010 <frankzhou1010@gmail.com>
2026-03-26 12:58:33 -0400

    updated schema for shopping cart, minor changes to other schemas

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS Users ( -- all create should check whether table exists
     email TEXT PRIMARY KEY,
     password TEXT NOT NULL
 );
@@ -12,12 +12,13 @@ CREATE TABLE IF NOT EXISTS Helpdesk (
 );
 
 CREATE TABLE IF NOT EXISTS Requests (
-    request_id INTEGER PRIMARY KEY,
+    request_id INTEGER,
     sender_email TEXT NOT NULL,
     helpdesk_staff_email TEXT NOT NULL,
     request_type TEXT NOT NULL,
     request_desc TEXT,
-    request_status INTEGER DEFAULT 0,
+    request_status INTEGER DEFAULT 0, -- we want the initial status of requests to be 0, meaning no request
+    PRIMARY KEY (request_id),
     FOREIGN KEY (sender_email) REFERENCES Users(email),
     FOREIGN KEY (helpdesk_staff_email) REFERENCES Helpdesk(email)
 );
@@ -29,20 +30,22 @@ CREATE TABLE IF NOT EXISTS Zipcode_Info (
 );
 
 CREATE TABLE IF NOT EXISTS Address (
-    address_id TEXT PRIMARY KEY,
+    address_id TEXT,
     zipcode TEXT NOT NULL,
     street_num TEXT,
     street_name TEXT,
+    PRIMARY KEY (address_id),
     FOREIGN KEY (zipcode) REFERENCES Zipcode_Info(zipcode)
 );
 
 CREATE TABLE IF NOT EXISTS Bidders (
-    email TEXT PRIMARY KEY,
+    email TEXT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     age INTEGER,
     home_address_id TEXT,
     major TEXT,
+    PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES Users(email),
     FOREIGN KEY (home_address_id) REFERENCES Address(address_id)
 );
@@ -58,25 +61,28 @@ CREATE TABLE IF NOT EXISTS Credit_Cards (
 );
 
 CREATE TABLE IF NOT EXISTS Sellers (
-    email TEXT PRIMARY KEY,
+    email TEXT,
     bank_routing_number TEXT,
     bank_account_number TEXT,
     balance REAL DEFAULT 0,
+    PRIMARY KEY (email),
     FOREIGN KEY (email) REFERENCES Users(email)
 );
 
 CREATE TABLE IF NOT EXISTS Local_Vendors (
-    Email TEXT PRIMARY KEY,
+    Email TEXT,
     Business_Name TEXT NOT NULL,
     Business_Address_ID TEXT,
     Customer_Service_Phone_Number TEXT,
+    PRIMARY KEY (Email),
     FOREIGN KEY (Email) REFERENCES Sellers(email),
     FOREIGN KEY (Business_Address_ID) REFERENCES Address(address_id)
 );
 
 CREATE TABLE IF NOT EXISTS Categories (
-    category_name TEXT PRIMARY KEY,
-    parent_category TEXT
+    category_name TEXT,
+    parent_category TEXT,
+    PRIMARY KEY (category_name)
 );
 
 CREATE TABLE IF NOT EXISTS Auction_Listings (
@@ -89,6 +95,11 @@ CREATE TABLE IF NOT EXISTS Auction_Listings (
     Quantity INTEGER DEFAULT 1,
     Reserve_Price REAL,
     Max_bids INTEGER,
+    remaining_bids INTEGER, --the number of bids on the product during the time of removal, if removed
+    reason_of_removal TEXT, -- added a reason of removal from the auction listing
+    is_promoted INTEGER DEFAULT 0, -- binary for whether the product is under promotion or not, default 0 for no
+    promotion_fee REAL,
+    promotion_time TIME,
     Status INTEGER DEFAULT 1,
     PRIMARY KEY (Seller_Email, Listing_ID),
     FOREIGN KEY (Seller_Email) REFERENCES Sellers(email),
@@ -96,22 +107,24 @@ CREATE TABLE IF NOT EXISTS Auction_Listings (
 );
 
 CREATE TABLE IF NOT EXISTS Bids (
-    Bid_ID INTEGER PRIMARY KEY,
+    Bid_ID INTEGER,
     Seller_Email TEXT NOT NULL,
     Listing_ID INTEGER NOT NULL,
     Bidder_Email TEXT NOT NULL,
     Bid_Price REAL NOT NULL,
+    PRIMARY KEY (Bid_ID),
     FOREIGN KEY (Seller_Email, Listing_ID) REFERENCES Auction_Listings(Seller_Email, Listing_ID),
     FOREIGN KEY (Bidder_Email) REFERENCES Bidders(email)
 );
 
 CREATE TABLE IF NOT EXISTS Transactions (
-    Transaction_ID INTEGER PRIMARY KEY,
+    Transaction_ID INTEGER,
     Seller_Email TEXT NOT NULL,
     Listing_ID INTEGER NOT NULL,
     Buyer_Email TEXT NOT NULL,
     Date TEXT,
     Payment REAL,
+    PRIMARY KEY (Transaction_ID),
     FOREIGN KEY (Seller_Email, Listing_ID) REFERENCES Auction_Listings(Seller_Email, Listing_ID),
     FOREIGN KEY (Buyer_Email) REFERENCES Bidders(email)
 );
@@ -129,24 +142,36 @@ CREATE TABLE IF NOT EXISTS Rating (
 
 -- Team Phase 1 new feature: Product Q&A
 CREATE TABLE IF NOT EXISTS Questions (
-    question_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question_id INTEGER AUTOINCREMENT,
     Seller_Email TEXT NOT NULL,
     Listing_ID INTEGER NOT NULL,
     Bidder_Email TEXT NOT NULL,
     question_text TEXT NOT NULL,
     answer_text TEXT,
     question_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (question_id),
     FOREIGN KEY (Seller_Email, Listing_ID) REFERENCES Auction_Listings(Seller_Email, Listing_ID),
     FOREIGN KEY (Bidder_Email) REFERENCES Bidders(email)
 );
 
 -- Team Phase 1 new feature: Watchlist
 CREATE TABLE IF NOT EXISTS Watchlist (
-    watchlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Bidder_Email TEXT NOT NULL,
+    watchlist_id INTEGER AUTOINCREMENT,
+    Bidder_email TEXT NOT NULL,
     category TEXT,
-    max_price REAL,
-    condition TEXT,
+    max_price REAL, --maximum price the user wants to keep track of the product until
+    condition TEXT, --condition of the product that you want it in
+    PRIMARY KEY (watchlist_id),
     FOREIGN KEY (Bidder_Email) REFERENCES Bidders(email),
     FOREIGN KEY (category) REFERENCES Categories(category_name)
 );
+
+CREATE TABLE IF NOT EXISTS Shopping_Cart(
+    cart_id INTEGER,
+    bidder_email TEXT NOT NULL,
+    seller_email TEXT NOT NULL,
+    Listing_ID INTEGER NOT NULL,
+    PRIMARY KEY (cart_id),
+    FOREIGN KEY (Bidder_email) REFERENCES Bidders(email),
+    FOREIGN KEY (Seller_Email, Listing_ID) REFERENCES Auction_Listings(Seller_Email, Listing_ID)
+)
